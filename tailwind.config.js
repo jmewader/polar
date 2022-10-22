38f63a4fceaa6117e3a073416bee7daa35edc185
Настя <smelw@list.ru>
2022-10-23 00:29:25 +0300

    changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html'],
+  content: ["./*.html"],
   theme: {
-    extend: {},
+    screens: {
+      ld: { max: "1200xp" },
+      md: { max: "900px" },
+      sm: { max: "500px" },
+    },
+    container: {
+      padding: "20px",
+      center: true,
+    },
+    extend: {
+      colors: {
+        darkPink: "#CF5979",
+        lightBlack: "#101010",
+      },
+    },
   },
-  plugins: [],
-}
+  plugins: {
+    "postcss-import": {},
+    "tailwindcss/nesting": {},
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
